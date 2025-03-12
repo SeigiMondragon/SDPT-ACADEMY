eed33535633bf27bb3de081d424e421be1d38e9c
SeigiMondragon <sonic92yeylol@gmail.com>
2025-03-13 01:39:35 +0800

    AboutUs

---
+++ SDPT Academy/src/components/AboutUs/MainAbout.jsx
@@ -0,0 +1,17 @@
+import IntroductionHome from "./IntroductionHome.jsx"
+import NavBar from "../Navbar/Navbar.jsx"
+
+
+function MainAboutUs() {
+
+    return(
+      <>
+        <NavBar/>
+        <IntroductionHome/>
+      </>
+
+    )
+
+}
+
+export default MainAboutUs
