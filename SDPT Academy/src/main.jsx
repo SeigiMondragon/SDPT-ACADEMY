24621cf7054acd14ee7cc15441c4a47542c20453
SamuelAlac <samuelalac21@gmail.com>
2025-03-13 01:51:29 +0800

    app

--- SDPT Academy/src/main.jsx
+++ SDPT Academy/src/main.jsx
@@ -9,7 +9,7 @@ import Courses from './components/CoursesCatalog/Courses.jsx';
 const Router = createBrowserRouter([
   {
     path: "/",
-    element: <Courses/>
+    element: <App/>
   },
   {
     path: "Home",
