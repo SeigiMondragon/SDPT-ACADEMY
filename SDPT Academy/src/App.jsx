dbad192ec315a0cf23e3e388cef1f17e5f303e58
ver09 <oliver.baldomero09@gmail.com>
2025-03-12 22:31:21 +0800

    f

--- SDPT Academy/src/App.jsx
+++ SDPT Academy/src/App.jsx
@@ -1,14 +1,13 @@
 import { useState } from 'react'
-
+import test from './assets/sdpt.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
-       SDPT
+       <img src= {test} alt="" />
       </div>
     </>
   )
