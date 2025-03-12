276c4da49ff097d1062fabb0cb78b80110eb989e
ver09 <oliver.baldomero09@gmail.com>
2025-03-12 23:10:01 +0800

    navbar

--- SDPT Academy/src/components/Navbar/Navbar.jsx
+++ SDPT Academy/src/components/Navbar/Navbar.jsx
@@ -1,14 +1,15 @@
-import Logo from '../../assets/sdpt.svg'
+import Logo from '../../assets/SDPT Nav Logo.svg'
 import HomeIcon from '../../assets/Home Icon.svg'
 import AboutIcon from '../../assets/About Icon.svg'
 import CoursesIcon from '../../assets/Courses Icon.svg'
 import ProfileIcon from '../../assets/Profile Icon.svg'
+import './Navbar.css'
 
 
 function NavBar({ useHomeBG }) {
   return (
     <div className="header">
-      
+        {useHomeBG && <div className="header-bg"></div>}
       <div className="nav-container">
         <img className="header-logo" src={Logo} alt="Logo" />
         <div className="navlinks-container">
@@ -16,7 +17,6 @@ function NavBar({ useHomeBG }) {
             <img className="nav-icon" src={AboutIcon} alt="About Us" />      
             <img className="nav-icon" src={CoursesIcon} alt="Courses" />        
             <img className="nav-icon-profile" src={ProfileIcon} alt="Profile" />  
-            <img src= {profile} alt="" />
         </div>
       </div>
     </div>
