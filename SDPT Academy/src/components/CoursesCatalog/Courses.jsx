cdc641ce63a1b87f7316aa579b98a4cfb63ec788
SamuelAlac <samuelalac21@gmail.com>
2025-03-13 01:42:57 +0800

    sam

--- SDPT Academy/src/components/CoursesCatalog/Courses.jsx
+++ SDPT Academy/src/components/CoursesCatalog/Courses.jsx
@@ -6,7 +6,57 @@ function Courses (){
   return (
     <>
       <NavBarBorder />
-      <div>Course 1</div>
+            {/* <div id="carouselExample" class="carousel slide">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+            <img src="..." class="d-block w-100" alt="..."/>
+            </div>
+            <div class="carousel-item">
+            <img src="..." class="d-block w-100" alt="..."/>
+            </div>
+            <div class="carousel-item">
+            <img src="..." class="d-block w-100" alt="..."/>
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+
+        <div>
+            Featured Courses
+        </div>
+
+        <div>
+        <form class="d-flex" role="search">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+            <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+            Courses
+        </div>
+        </div> */}
+        <div>
+            <div class = "container ">
+            <button type="button" class="btn btn-outline-warning">Buy Courses</button>
+            <button type="button" class="btn btn-outline-warning">Owned Courses</button>
+            </div>
+
+            <div class = "container">
+                <div class = "row">
+                    <div class = "col">
+                        <p>test</p>
+                    </div>
+
+                    <div class = "col">
+                    <p>test</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </>
   )
 }
